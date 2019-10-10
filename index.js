9e1b045c9841b37abea1f85a4eb6dd3ed54d6bf7
Benji Lewis <benji@benjisoft.org.uk>
2019-10-10 15:55:50 +0100

    Added cooldown

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+var lastFired
 
 client.on("message", async message => {
-    message.react(config.reaction)
+    currentTime =  Math.round(now.getTime() / 1000);
+    timeSince = lastFired - currentTime;
+    if (timeSince > config.minTime) {
+        message.react(config.reaction)
+        lastFired = currentTime;
+    };
 });
 
 client.login(config.token);
